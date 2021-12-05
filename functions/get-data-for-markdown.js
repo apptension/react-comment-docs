055aa5c184fd4ecee0fd2e74166fee2b6d436812
Igor Binkowski <ibinkowski@apptension.com>
2021-12-05 15:12:36 +0100

    added ts types support to getDataForMarkdown

--- functions/get-data-for-markdown.js
+++ functions/get-data-for-markdown.js
@@ -1,27 +1,42 @@
 import delve from "dlv";
 import { readFileSync } from "fs";
 
-
 export const getDataForMarkdown = (path, fields) => {
   const fileContent = readFileSync(path, "utf8");
   const data = {};
-  const tsReg = new RegExp(`interface .* {.*^}`, 'gms');
+  const tsReg = new RegExp(`interface .* {(?<types>[^}]+)^}`, "ms");
   const tsMatch = fileContent.match(tsReg);
-  // Typescript support 
-  if(fileContent.match(tsReg)) {
-    console.log(path ,'this project is using typescript!')
+  // Typescript support
+  if (tsMatch) {
+    const tsTypesObj = {};
+    let tsTypes = delve(tsMatch, `groups.types`, null);
+    if (!tsTypes) return;
+    tsTypes = tsTypes.split(";");
+    tsTypes = tsTypes.filter((type) => {
+      return type.indexOf(":") != -1;
+    });
+    for (const tsType of tsTypes) {
+      const [key, value] = tsType.split(":");
+      tsTypesObj[key.trim()] = value.trim();
+    }
+    data.tsTypes = tsTypesObj;
   }
 
   fields.forEach((field) => {
-    // Match fields 
-    const fieldReg = new RegExp(`@docs ${field}(?<${field}>.*)!docs ${field}`, 's');
+    // Match fields
+    const fieldReg = new RegExp(
+      `@docs ${field}(?<${field}>.*)!docs ${field}`,
+      "s"
+    );
     const match = fileContent.match(fieldReg);
     let text = delve(match, `groups.${field}`, null);
     if (!text) return;
-    const formattedText = text.replace(/^\s+\/\/ [@!]docs.*$\n/mg, "").replace(/^\s+\/\/.*$/mg, "");
+    const formattedText = text
+      .replace(/^\s+\/\/ [@!]docs.*$\n/gm, "")
+      .replace(/^\s+\/\/.*$/gm, "");
     data[field] = formattedText;
   });
 
-  // console.log('DATA',data)
+  console.log('DATA',data)
   return data;
 };
